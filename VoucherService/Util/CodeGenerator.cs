58ca12f18b5980b12dc523545fcbbf65a0e7a928
Agboola Emmanuel Olorunleke <olorunlexe@gmail.com>
2019-01-14 19:17:11 +0100

    Documentation added to code generator

--- VoucherService/Util/CodeGenerator.cs
+++ VoucherService/Util/CodeGenerator.cs
@@ -8,8 +8,13 @@ namespace VoucherService.Util
 {
     public class CodeGenerator
     {
-
-        private static string GenerateCode(int length, string characters)
+        /// <summary>
+        /// Create a code with specific length and Character set
+        /// </summary>
+        /// <param name="length">the number of characters to be Generated e.g A code with length of 5 (12345)</param>
+        /// <param name="characterSet">A pool of Characters to generate number from</param>
+        /// <returns></returns>
+        private static string GenerateCode(int length, string characterSet)
         {
             Random random = new Random();
             StringBuilder result;
@@ -17,19 +22,30 @@ namespace VoucherService.Util
             result = new StringBuilder(length);
             for (int i = 0; i < length; i++)
             {
-                result.Append(characters[random.Next(characters.Length)]);
+                result.Append(characterSet[random.Next(characterSet.Length)]);
             }
             return result.ToString();
         }
 
+        /// <summary>
+        /// Add defined Prefix to generated code
+        /// </summary>
+        /// <param name="prefix">Given set of Strings to be Concatenated to the beginning of Generated Code</param>
+        /// <param name="code">A system of characters, letters or symbols used for representation</param>
+        /// <returns></returns>
         public static string GetCodeWithPrefix(string prefix, string code)
         {
             return prefix + code;
         }
 
+        /// <summary>
+        /// Add defined Suffix to generated code
+        /// </summary>
+        /// <param name="code">A system of characters, letters or symbols used for representation</param>
+        /// <param name="suffix">Given set of Strings to be Concatenated at the end of Generated Code</param>
+        /// <returns></returns>
         public static string GetCodeWithSuffix(string code, string suffix)
         {
-            //string characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
             return code + suffix;
 
         }
