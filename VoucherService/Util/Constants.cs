b2b759a7a96836188107590d0014bf429bd2bc3b
gitdunamis <johnsimon6000@hotmail.com>
2019-01-15 08:06:11 +0100

    Update Constants declarations

--- VoucherService/Util/Constants.cs
+++ VoucherService/Util/Constants.cs
@@ -5,8 +5,5 @@ namespace VoucherService.Util
     {
         public static readonly string AlphabetCharacters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
         public static readonly string NumberCharacters = "0123456789";
-
-        public static readonly string SpecialCharacters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-
     }
 }
