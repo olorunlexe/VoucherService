b7c86fa6ec14a15a453edd73fceca8c1e4078196
gitduNaMis <johnsimon6000@hotmail.com>
2019-01-16 10:46:55 +0100

    Readded Util

---
+++ VoucherServiceBL/Util/Constants.cs
@@ -0,0 +1,9 @@
+
+namespace VoucherService.Util
+{
+    public static class Constants
+    {
+        public static readonly string AlphabetCharacters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+        public static readonly string NumberCharacters = "0123456789";
+    }
+}
