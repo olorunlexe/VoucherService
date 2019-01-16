d06e05270496a72bf02e651daa7cb4784d925f1d
Agboola Emmanuel Olorunleke <olorunlexe@gmail.com>
2019-01-16 10:38:41 +0100

    Recreated

--- VoucherServiceBL/Util/Constants.cs
+++
@@ -1,9 +0,0 @@
-
-namespace VoucherService.Util
-{
-    public static class Constants
-    {
-        public static readonly string AlphabetCharacters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
-        public static readonly string NumberCharacters = "0123456789";
-    }
-}
