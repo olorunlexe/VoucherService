93296de78b71268c12e1db8fb4c5a04bee8bb089
Abubakar Zainab <abuzain1995@yahoo.com>
2019-01-21 10:51:45 +0100

    Changes to value voucher service

--- VoucherServiceBL/Domain/Value.cs
+++ VoucherServiceBL/Domain/Value.cs
@@ -9,7 +9,7 @@ namespace VoucherServiceBL.Domain
     [Table("ValueVoucher")]
     public class Value:Voucher
     {
-        public long valueAmount { get; set; }
+        public long ValueAmount { get; set; }
         public string status { get; set; }
     }
 }
---
+++ VoucherServiceBL/Service/ValueVoucher.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using VoucherService.Util;
+using VoucherServiceBL.Domain;
+using VoucherServiceBL.Model;
+using VoucherServiceBL.ValueVoucher;
+using VoucherServiceBL.ValueVoucher.Repository;
+
+namespace VoucherServiceBL.Service
+{
+    class ValueVoucher : IValueVoucher
+    {
+        public IValueRepository Repository;
+
+        public ValueVoucher(IValueRepository repository)
+        {
+            this.Repository = repository;
+        }
+
+        public Value CreateValueVoucher(VoucherRequest valueRequest)
+        {
+            //create the gift object from the Vouher
+            Value giftVoucher;
+            giftVoucher = new Value()
+            {
+                Code = HashedCode(valueRequest),
+                CreationDate = valueRequest.CreationDate,
+                ExpiryDate = valueRequest.ExpiryDate,
+                VoucherStatus = "Active",
+                VoucherType = valueRequest.VoucherType,
+                Description = valueRequest.Description,
+                ValueAmount = valueRequest.ValueAmount,
+                MerchantId = valueRequest.MerchantId,
+                Metadata = valueRequest.Metadata,
+            };
+
+            //persist the object to the db    
+            return Repository.CreateValueVoucher(giftVoucher);
+        }
+
+        public List<Value> GetAllValueVouchers()
+        {
+            throw new NotImplementedException();
+        }
+        
+    }
+}
