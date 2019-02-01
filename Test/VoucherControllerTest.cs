852ef856d22d42ab1e7b67d7c91486e96452d9cc
Abubakar Zainab <abuzain1995@yahoo.com>
2019-02-01 15:59:55 +0100

    stored procedure changes

--- Test/VoucherControllerTest.cs
+++ Test/VoucherControllerTest.cs
@@ -1,3 +1,4 @@
+//using Microsoft.AspNetCore.Mvc;
 //using Moq;
 //using NUnit.Framework;
 //using System;
@@ -22,94 +23,123 @@
 //        public async Task GetVoucher_WhenCalledWithCorrectCode_ShouldReturnCorrectVoucher()
 //        {
 
+//            //// Arrange
+//            //var testVoucher = GetVoucher();
+//            //var mockService = new Mock<IVoucherService>();
+//            //mockService.Setup(service => service.GetVoucherByCode("25142dw")).Returns(testVoucher);
+//            //var controller = new VoucherController(mockService.Object);
+
+//            //// Act
+//            //var result = await controller.GetVoucher("25142dw");
+
+//            //// Assert
+//            //Assert.IsNotNull(result);
+//            //Assert.AreEqual(testVoucher, result);
+
 //            // Arrange
-//            var testVoucher = GetVoucher();
+//            //var testVoucher = GetVoucher();
+//            var testVoucher = "25142dw";
 //            var mockService = new Mock<IVoucherService>();
-//            mockService.Setup(service => service.GetVoucherByCode("25142dw")).Returns(testVoucher);
+//            mockService.Setup(service => service.GetVoucherByCode(testVoucher))
+//                .ReturnsAsync(GetVoucher());
+
 //            var controller = new VoucherController(mockService.Object);
 
 //            // Act
-//            var result = await controller.GetVoucher("25142dw");
- 
+//            ActionResult<Voucher> result = await controller.GetVoucher("25142dw");
+//            //var result = await controller.GetVoucher("25142dw");
+
 //            // Assert
-//            Assert.IsNotNull(result);
-//            Assert.AreEqual(testVoucher, result);
+//            //Assert.IsNotNull(actionResult);
+//            //Assert.IsNotNull(actionResult.Content);
+//            //Assert.AreEqual(42, contentResult.Content.Id);
+//            var actionResult = Assert.IsInstanceOf<ActionResult<Voucher>>(result);
+//            var returnValue = Assert.IsInstanceOf<Voucher>(actionResult.Value);
+//            var idea = returnValue.FirstOrDefault();
+//            Assert.AreEqual("2222", idea.);
 
 //        }
 
-//        [Test]
-//        public async Task GetVoucher_WhenCalledWithWrongCode_ShouldNotReturnNull()
-//        {
-//            //Arrange
-//            var testVoucher = GetVoucherByCode("sgwt3683ydg");
-//            var mockService = new Mock<IVoucherService>();
-//            mockService.Setup(service => service.GetVoucherByCode("sgwt3683ydg")).Returns(testVoucher);
-//            var controller = new VoucherController(mockService.Object);
 
-//            //Act
-//            var result = await controller.GetVoucher("sgwt3683ydg");
+//        //GetVoucher_WhenCalledWithWrongCode_ShouldNotReturnNull() test confirms that the controller 
+//        //returns an ActionResult but not a nonexistent list of ideas for a nonexistent voucher:
 
-//            //Assert
-//            Assert.IsNull(result);
-//            Assert.AreEqual(testVoucher, result);
-//        }
+//        //The ActionResult type is ActionResult<Voucher>.
+//        //The Result is a NotFoundResult.
 
 //        [Test]
-//        public async Task GetAllVoucher_WhenCalledWithCorrectMerchantId_ShouldReturnAListOfAllVouchersAttachedToTheMerchant()
+//        public async Task GetVoucher_WhenCalledWithWrongCode_ReturnsNotFoundResultForNonexistentVoucher()
 //        {
 
 //            // Arrange
-//            var testVouchers = GetVouchersByMerchantId("2222");
-//            var mockService = new Mock<IVoucherService>();
-//            mockService.Setup(service => service.GetAllVouchers("2222")).Returns (testVouchers);
-//            var controller = new VoucherController(mockService.Object);
+//            var mockRepo = new Mock<IVoucherService>();
+//            var controller = new VoucherController(mockRepo.Object);
+//            var nonExistentVoucher = "sr4579ndsh";
 
 //            // Act
-//            var result = await controller.GetAllVouchers("2222");
+//            ActionResult<Voucher> actionResult = await controller.GetVoucher(nonExistentVoucher);
 
 //            // Assert
-//            Assert.IsNotNull(result);
-//            Assert.AreEqual(testVouchers, result);
-//            Assert.AreEqual(2, result.Count());
+//            Assert.IsInstanceOf<ActionResult<Voucher>>(actionResult);
+//            Assert.IsInstanceOf<NotFoundResult>(actionResult.Result);
 //        }
-
-//        [Test]
-//        public async Task GetAllVoucher_WhenCalledWithWrongMerchantId_ShouldReturnAnyVoucher()
-//        {
-
-//            // Arrange
-//            var testVouchers = GetVouchersByMerchantId("6234");
-//            var mockService = new Mock<IVoucherService>();
-//            mockService.Setup(service => service.GetAllVouchers("6234")).Returns(testVouchers);
-//            var controller = new VoucherController(mockService.Object);
-
-//            // Act
-//            var result = await controller.GetAllVouchers("6234");
-
-//            // Assert
-//            Assert.AreEqual(testVouchers, result);
-//            Assert.AreEqual(0, result.Count());
-//        }
-
-
 
 //        //[Test]
-//        //public async Task DeleteVoucher_ShouldChangeVoucherStatusToDeleted()
+//        //public async Task GetAllVoucher_WhenCalledWithCorrectMerchantId_ShouldReturnAListOfAllVouchersAttachedToTheMerchant()
 //        //{
+
 //        //    // Arrange
-//        //    var testVoucher = GetVoucher();
+//        //    var testVouchers = GetVouchersByMerchantId("2222");
 //        //    var mockService = new Mock<IVoucherService>();
-//        //    mockService.Setup(service => service.DeleteVoucher("25142dw"));
+//        //    mockService.Setup(service => service.GetAllVouchers("2222")).Returns(testVouchers);
 //        //    var controller = new VoucherController(mockService.Object);
 
 //        //    // Act
-//        //    var result = controller.DeleteVoucher("25142dw");
+//        //    var result = await controller.GetAllVouchers("2222");
 
 //        //    // Assert
 //        //    Assert.IsNotNull(result);
-//        //    Assert.AreEqual("Deleted", result.VoucherStatus);
-
+//        //    Assert.AreEqual(testVouchers, result);
+//        //    Assert.AreEqual(2, result.Count());
 //        //}
+
+//        //[Test]
+//        //public async Task GetAllVoucher_WhenCalledWithWrongMerchantId_ShouldReturnAnyVoucher()
+//        //{
+
+//        //    // Arrange
+//        //    var testVouchers = GetVouchersByMerchantId("6234");
+//        //    var mockService = new Mock<IVoucherService>();
+//        //    mockService.Setup(service => service.GetAllVouchers("6234")).Returns(testVouchers);
+//        //    var controller = new VoucherController(mockService.Object);
+
+//        //    // Act
+//        //    var result = await controller.GetAllVouchers("6234");
+
+//        //    // Assert
+//        //    Assert.AreEqual(testVouchers, result);
+//        //    Assert.AreEqual(0, result.Count());
+//        //}
+
+
+
+//        ////[Test]
+//        ////public async Task DeleteVoucher_ShouldChangeVoucherStatusToDeleted()
+//        ////{
+//        ////    // Arrange
+//        ////    var testVoucher = GetVoucher();
+//        ////    var mockService = new Mock<IVoucherService>();
+//        ////    mockService.Setup(service => service.DeleteVoucher("25142dw"));
+//        ////    var controller = new VoucherController(mockService.Object);
+
+//        ////    // Act
+//        ////    var result = controller.DeleteVoucher("25142dw");
+
+//        ////    // Assert
+//        ////    Assert.IsNotNull(result);
+//        ////    Assert.AreEqual("Deleted", result.VoucherStatus);
+
+//        ////}
 //        private IEnumerable<Value> GetVouchers()
 //        {
 //            var TestValueVouchers = new List<Value>();
@@ -139,6 +169,6 @@
 //            var value = from v in values where v.MerchantId == merchantId select v;
 //            return value.AsEnumerable<Value>();
 //        }
-        
+
 //    }
 //}
